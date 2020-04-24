5c05193c31677aa8ee0ecf41ddb0daf5afd5e8c1
Prabhat785 <prabhat.7858@gmail.com>
2020-04-25 00:28:03 +0530

    1st commit

--- app/src/main/java/com/example/rha/Login.java
+++ app/src/main/java/com/example/rha/Login.java
@@ -58,7 +58,7 @@ public class Login extends AppCompatActivity {
               }
               else
               {
-                  Toast.makeText(Login.this,"Authentication Error",Toast.LENGTH_SHORT).show();
+                  Toast.makeText(Login.this,"Wrong Password",Toast.LENGTH_SHORT).show();
               }
           }
       });
