b8384435eeb06a47d935296eec4bebfc079ca47c
tanishq-123 <tanishqb212000@gmail.com>
2020-04-28 11:05:42 +0530

    6th commit

--- app/src/main/java/com/example/rha/MainActivity.java
+++ app/src/main/java/com/example/rha/MainActivity.java
@@ -1,27 +1,57 @@
 package com.example.rha;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
-    SharedPreferences prf;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SharedPreferences prf;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private TextView textView;
+    private NavigationView navigationView;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+        navigationView=(NavigationView) findViewById(R.id.nav_view);
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle actionBarDrawerToggle=new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.opennavigationdrawer,
+                R.string.closennavigationdrawer
+                );
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        View header=navigationView.getHeaderView(0);
+        textView=header.findViewById(R.id.user_text);
+        prf = getSharedPreferences("user_details",MODE_PRIVATE);
+        textView.setText("Hello  "+prf.getString("username",null));
+        //textView=(TextView)findViewById(R.layout.nav_header_layout);
         TextView result = (TextView)findViewById(R.id.resultView);
         Button btnLogOut = (Button)findViewById(R.id.LogoutButton);
-        prf = getSharedPreferences("user_details",MODE_PRIVATE);
+
        final Intent intent = new Intent(MainActivity.this,Login.class);
-        result.setText("Hello, "+prf.getString("username",null));
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,5 +62,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.my_info:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame,new MyInfoFragement()).commit();
+                break;
+            case R.id.drives_history:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame,new DrivesFragement()).commit();
+                break;
+            case R.id.log:
+                logout();
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    private void logout(){
+        final Intent intent = new Intent(MainActivity.this,Login.class);
+        SharedPreferences.Editor editor = prf.edit();
+        editor.clear();
+        editor.commit();
+        startActivity(intent);
+        finish();
     }
 }
