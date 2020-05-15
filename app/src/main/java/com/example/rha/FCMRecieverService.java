d328bbf04405cd045a5193c015050daa2501525a
tanishq-123 <tanishqb212000@gmail.com>
2020-05-16 00:50:33 +0530

    Notification update

---
+++ app/src/main/java/com/example/rha/FCMRecieverService.java
@@ -0,0 +1,116 @@
+package com.example.rha;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+import static com.example.rha.App.FCM_CHANNEL_ID;
+
+public class FCMRecieverService extends FirebaseMessagingService {
+    private DatabaseReference userref,tokenref;
+    String currentuserid;
+
+    private FirebaseAuth mAuth;
+    @Override
+    public void onMessageReceived(@NonNull final RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        final String notificationtype=remoteMessage.getData().get("notificationType");
+        mAuth = FirebaseAuth.getInstance();
+        currentuserid = mAuth.getCurrentUser().getUid();
+        userref = FirebaseDatabase.getInstance().getReference().child("User").child(currentuserid);
+        userref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(notificationtype.equals("DriveNotification")){
+                    String pid=remoteMessage.getData().get("Sender");
+                    String NOTIFICATION_TITLE=remoteMessage.getData().get("pTitle");
+                    String NOTIFICATION_MESSAGE=remoteMessage.getData().get("pDescription");
+                        showpostnotification(pid,NOTIFICATION_TITLE,NOTIFICATION_MESSAGE);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+        if(remoteMessage.getNotification()!=null){
+            String title=remoteMessage.getNotification().getTitle();
+            String body=remoteMessage.getNotification().getBody();
+            Notification notification=new NotificationCompat.Builder(this,FCM_CHANNEL_ID).setSmallIcon(R.drawable.rha).
+                    setContentTitle(title).setContentText(body).setColor(Color.GREEN).build();
+            NotificationManager notificationManager=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(1002,notification);
+        }
+        if(remoteMessage.getData()!=null){
+
+        }
+
+    }
+
+    private void showpostnotification(String pid, String notification_title, String notification_message) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        int NotificationID=new Random().nextInt(100000);
+        if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    FCM_CHANNEL_ID, "Drives Notification", NotificationManager.IMPORTANCE_HIGH
+            );
+
+            notificationManager.createNotificationChannel(notificationChannel);
+            Intent intent=new Intent(this,MainActivity.class);
+            PendingIntent pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+            NotificationCompat.Builder notificationBuilder=new NotificationCompat.Builder(this, FCM_CHANNEL_ID).setSmallIcon(R.drawable.rha).
+                    setContentTitle(notification_title).setContentText(notification_message).setColor(Color.GREEN).setContentIntent(pendingIntent);
+            notificationManager.notify(1002,notificationBuilder.build());
+        }
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
+    }
+
+    @Override
+    public void onNewToken(@NonNull final String s) {
+        mAuth = FirebaseAuth.getInstance();
+        currentuserid = mAuth.getCurrentUser().getUid();
+        userref = FirebaseDatabase.getInstance().getReference().child("User").child(currentuserid);
+        userref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String chapter=dataSnapshot.child("Chapter").getValue().toString();
+                tokenref= FirebaseDatabase.getInstance().getReference().child("Tokens").child(chapter);
+                tokenref.child(currentuserid).setValue(s);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        super.onNewToken(s);
+    }
+}
